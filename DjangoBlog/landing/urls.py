6f78b6d75e3a4e599263961f0026c074a3866588
sambo2021 <mohamedsamomahmoud@gmail.com>
2022-03-17 16:19:18 -0400

    add log in form

--- DjangoBlog/landing/urls.py
+++ DjangoBlog/landing/urls.py
@@ -2,7 +2,7 @@ from  django.urls import path,include
 from  landing.views import Index 
 
 urlpatterns = [
-    path('',Index.as_view(),name='name'),
+    path('',Index.as_view(),name='index'),
    
 ]
 
