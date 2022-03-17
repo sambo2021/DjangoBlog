d5d35d49ff4463ae292756ff1bbd5533baa33952
sambo2021 <mohamedsamomahmoud@gmail.com>
2022-03-17 15:18:28 -0400

    initializing html of lading page

--- DjangoBlog/landing/views.py
+++ DjangoBlog/landing/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.views import View
 # Create your views here.
 class Index(View):
-    def get(self,request,*args, **kwargs ):
+    def get(self,request ):
         return render(request,'landing/index.html')
-        
+         
